2469f256cfc6658fa396ee11cc4f02585b7768fb
mwm1c <zzk13872229834@163.com>
2025-08-24 16:29:17 +0800

    complete LRU-v1

---
+++ LRUCache.h
@@ -0,0 +1,163 @@
+#pragma once
+
+#include <cstring>
+#include <list>
+#include <memory>
+#include <mutex>
+#include <unordered_map>
+#include "CachePolicy.h"
+
+namespace mwm1cCache
+{
+    template <typename Key, typename Value>
+    class LruCache;
+
+    template <typename Key, typename Value>
+    class LruNode
+    {
+    private:
+        Key key_;
+        Value value_;
+        size_t accessCount_;
+        std::weak_ptr<LruNode<Key, Value>> prev_;
+        std::shared_ptr<LruNode<Key, Value>> next_;
+
+    public:
+        LruNode(Key key, Value value)
+            : key_(key), value_(value), accessCount(1)
+        {
+        }
+        Key getKey() const { return key_ };
+        Value getValue() const { return value_ };
+        void setValue(const Value &value) { value_ = value };
+        size_t getAccessCount() const { return accessCount_ };
+        void incrementAccessCount() { ++accessCount_ };
+
+        friend class LruCache<Key, Value>;
+    };
+
+    // version 1
+    template <typename Key, typename Value>
+    class LruCache : public CachePolicy<Key, Value>
+    {
+    public:
+        using LruNodeType = LruNode<Key, Value>;
+        using NodePtr = std::shared<LruNodeType>; // be careful
+        using NodeMap = std::unordered_map<Key, NodePtr>;
+        LruCache(int cap) : capacity_(cap)
+        {
+            initializeList();
+        }
+        ~LruCache() override = default;
+        void put(Key key, Value value) override
+        {
+            if (capacity_ <= 0)
+                return;
+            std::lock_guard<std::mutex> lock(mutex_);
+            auto it = nodeMap_.find(key);
+            if (it != nodeMap_.end())
+            {
+                /**
+                 * If the key is in the current container, update the value and call the get method,
+                 * indicating that the data has just been accessed.
+                 */
+                updateExistingNode(it->second, value);
+                return;
+            }
+            addNewNode(key, value);
+        }
+        bool get(Key key, Value &value) override
+        {
+            std::lock_guard(mutex_);
+            auto it = nodeMap_.find(key);
+            if (it != nodeMap.end())
+            {
+                moveToMostRecent(it->second);
+                value = it->second->getValue();
+                return true;
+            }
+            return false;
+        }
+        Value get(Key key) override
+        {
+            Value value{};
+            get(key, value);
+            return value;
+        }
+        void remove(Key key)
+        {
+            std::lock_guard<std::mutex> lock(mutex_);
+            auto it = nodeMap_.find(key);
+            if (it != nodeMap_.end())
+            {
+                removeNode(it->second);
+                nodeMap_.erase(it);
+            }
+        }
+
+    private:
+        void initialize()
+        {
+            dummyHead_ = std::make_shared<LruNodeType>(Key(), Value());
+            dummyTail_ = std::make_shared<LruNodeType>(Key(), Value());
+            dummyHead_->next = dummyTail_;
+            dummyTail_->next = dummyHead_;
+        }
+        void updateExistingNode(NodePtr node, const Value &value)
+        {
+            node->setValue(value);
+            moveToMostRecent(node);
+        }
+        void addNewNode(const Key &key, const Value &value)
+        {
+            if (nodeMap_.size() >= capacity_)
+            {
+                evictLeastRecent();
+            }
+            // NodePtr newNode = std::make_shared<NodePtr>(Key(key), Value(value));
+            NodePtr newNode = std::make_shared<LruNodeType>(key, value);
+            insertNode(newNode);
+            nodeMap_[key] = newNode;
+        }
+        void moveToMostRecent(NodePtr node)
+        {
+            removeNode(node);
+            insertNode(node);
+        }
+        void removeNode(NodePtr node)
+        {
+            if (!node->prev.expired() && node->next_)
+            {
+                auto prev = node->prev_.lock();
+                prev->next_ = node->next_;
+                node->next_->prev_ = prev;
+                node->next_ = nullptr;
+            }
+        }
+        // insert node to tail of linkedlist
+        void insertNode(NodePtr node)
+        {
+            node->next_ = dummyTail_;
+            node->prev_ = dummyTail_->prev_;
+            dummyTail_->prev_.lock()->next = node;
+            dummyTail_->prev_ = node;
+        }
+        void evictLeastRecent()
+        {
+            NodePtr leastRecent = dummyHead_->next_;
+            removeNode(leastRecent);
+            nodeMap_.erase(leastRecent->getKey());
+        }
+        int capacity_;
+        NodeMap nodeMap_;
+        std::mutex mutex_;
+        NodePtr dummyHead_;
+        NodePtr dummyTail_;
+    };
+
+    // version 2
+    template <typename Key, typename Value>
+    class LruKCache : public LruCache<Key, Value>
+    {
+    };
+}
